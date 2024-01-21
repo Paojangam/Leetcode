2be79432f9f51316e618a2c31ffaf6a49a0fc36b
Paojangam kipgen <paojangam1234@gmail.com>
2024-01-21 17:04:57 +0000

    [LeetCode Sync] Runtime - 12 ms (5.26%), Memory - 8.2 MB (99.99%)

---
+++ leetcode/0009-palindrome-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        } 
+        long long reversed=0;
+        long long temp=x;
+        while(temp!=0){
+            int degit=temp%10; 
+            reversed=(reversed*10)+degit;
+            temp=temp/10;}
+            return(x==reversed);}
+
+    };
