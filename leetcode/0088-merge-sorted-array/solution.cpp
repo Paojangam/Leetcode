60f231a5445782e1a147ac0f5890428a130869ae
Paojangam kipgen <paojangam1234@gmail.com>
2025-02-05 16:15:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (8.84%)

---
+++ leetcode/0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> combine; // Correctly declare a vector (not a pointer)
+        int i=0; int j=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                combine.push_back(nums1[i]);
+                i++;
+            }else{
+                combine.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<m){
+            combine.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n){
+            combine.push_back(nums2[j]);
+            j++;
+        }
+         for (int k = 0; k < m + n; k++) {
+            nums1[k] = combine[k];}
+
+    }
+};
+
