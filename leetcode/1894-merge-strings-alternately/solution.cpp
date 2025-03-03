aa27c71880fdd2e573c736e4423402481fe98eb9
Paojangam kipgen <paojangam1234@gmail.com>
2025-03-03 18:43:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.4 MB (83.20%)

---
+++ leetcode/1894-merge-strings-alternately/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string word3;
+        int i=0,j=0;
+        while(i<word1.length()&&j<word2.length()){
+            word3+=word1[i++];
+            word3+=word2[j++];
+        }
+        if(i<word1.length()) word3+=word1.substr(i);
+        if(j<word2.length()) word3+=word2.substr(j);
+         return word3;
+
+    }
+};
