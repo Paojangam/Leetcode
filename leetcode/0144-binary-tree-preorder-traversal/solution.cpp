4e489b9574d2128e8b4beb57e3fdb9a40343e4b9
Paojangam kipgen <paojangam1234@gmail.com>
2025-03-24 17:56:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (92.93%)

---
+++ leetcode/0144-binary-tree-preorder-traversal/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        st.push(root);
+        vector<int>result;
+                if(root==nullptr) return result;
+
+        TreeNode* node=root;
+        while(!st.empty()){
+             TreeNode* node=st.top();
+             st.pop();
+             result.push_back(node->val);
+             if(node->right!=NULL){
+                st.push(node->right);
+             }
+             if(node->left!=NULL){
+                st.push(node->left);
+             }
+
+            
+        }
+       return result; 
+    }
+};
