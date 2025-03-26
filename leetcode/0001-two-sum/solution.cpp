193fc2df69426af0553b266daadf72dcc2b73116
Paojangam kipgen <paojangam1234@gmail.com>
2025-03-26 16:11:31 +0000

    [LeetCode Sync] Runtime - 149 ms (5.03%), Memory - 14.1 MB (78.57%)

--- leetcode/0001-two-sum/solution.cpp
+++ leetcode/0001-two-sum/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int > hashmap;
-        for(int i=0; i<nums.size();i++){
-            int complement= target-nums[i];
-            if(hashmap.find(complement)!=hashmap.end()){
-                return { hashmap[complement],i};
-            }
-             hashmap[nums[i]] = i;
-        }
-
-return {};
+        vector<int>result;
+                    for(int i=0;i<nums.size();i++){
+                        for(int j=i+1;j<nums.size();j++){
+                            if(nums[i]+nums[j]==target){
+                           result.push_back(i);
+                           result.push_back(j);
+                            }else {
+                                continue;
+                            }
+                        }
+                    }
+                    return result;
     } 
 };
