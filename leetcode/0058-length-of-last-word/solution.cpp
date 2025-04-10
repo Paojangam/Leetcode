eae9af30deaaf5cb235c2e5dd995696f9d29d542
Paojangam kipgen <paojangam1234@gmail.com>
2025-04-10 13:32:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (32.59%)

--- leetcode/0058-length-of-last-word/solution.cpp
+++ leetcode/0058-length-of-last-word/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-        bool counting = false;
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s[i] != ' ') {
-                counting = true;
-                length++;
-            }
-            else if (counting) {
-                break;
-            }
-        }
-        
-        return length;
+        int length=0;
+    int right=s.length()-1;
+    while(right>=0 && s[right]==' '){
+        right--;
+    }
+    while(right>=0 && s[right]!=' '){
+        length++;
+        right--;
+    }
+    return length;
     }
 };
