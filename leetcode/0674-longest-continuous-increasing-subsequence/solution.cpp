acb25fee9447f20631fa2fc58c52119b44e3879a
Paojangam kipgen <paojangam1234@gmail.com>
2025-06-23 17:29:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.8 MB (63.23%)

---
+++ leetcode/0674-longest-continuous-increasing-subsequence/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+   int findLengthOfLCIS(vector<int>& nums) {
+    if (nums.empty()) return 0;
+
+    int count = 1, maxCount = 1;
+
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > nums[i - 1]) {
+            count++;
+        } else {
+            count = 1; 
+        }
+        maxCount = max(maxCount, count);
+    }
+    return maxCount;
+}
+
+};
