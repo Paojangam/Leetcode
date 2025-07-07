b3a1b1a213cdb100f7648330e6210237d0986bd5
Paojangam kipgen <paojangam1234@gmail.com>
2025-07-07 17:29:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (21.48%)

---
+++ leetcode/1764-maximum-repeating-substring/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        int k = 0;
+        string repeated = word;
+        
+        while (sequence.find(repeated) != string::npos) {
+            k++;
+            repeated += word; 
+        }
+        
+        return k;
+    }
+};
+
