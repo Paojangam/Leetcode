3c29269cd474109e59d3190fb37b8be35da8f385
Paojangam kipgen <paojangam1234@gmail.com>
2025-08-16 21:26:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.3 MB (90.38%)

--- leetcode/0206-reverse-linked-list/solution.cpp
+++ leetcode/0206-reverse-linked-list/solution.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;  // Previous pointer
-        ListNode* curr = head;     // Current pointer
-
-        while (curr != nullptr) {
-            ListNode* nextNode = curr->next; // Store next node
-            curr->next = prev;  // Reverse the link
-            prev = curr;  // Move prev forward
-            curr = nextNode;  // Move curr forward
+        ListNode*prev=nullptr;
+        ListNode*cur=head;
+        while(cur!=nullptr){
+            ListNode*nextnode=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=nextnode;
         }
-
-        return prev; // New head (last node in original list)
+        return prev;
     }
 };
 
