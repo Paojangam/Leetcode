e8ba02a957ca7bf49a4215c61a91d157bda90e52
Paojangam kipgen <paojangam1234@gmail.com>
2025-08-31 21:25:10 +0000

    [LeetCode Sync] Runtime - 166 ms (82.90%), Memory - 0.0B (100.00%)

---
+++ leetcode/1801-average-time-of-process-per-machine/solution.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+WITH process_durations AS (
+    SELECT
+        machine_id,
+        process_id,
+        MAX(CASE WHEN activity_type = 'end' THEN timestamp END) -
+        MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS duration
+    FROM Activity
+    GROUP BY machine_id, process_id
+)
+SELECT
+    machine_id,
+    ROUND(AVG(duration)::numeric, 3) AS processing_time
+FROM process_durations
+GROUP BY machine_id;
+
