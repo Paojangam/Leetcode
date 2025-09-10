3697540e6631cced4e16611cbda6b87e50edba71
Paojangam kipgen <paojangam1234@gmail.com>
2025-09-10 06:59:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.1 MB (17.83%)

--- leetcode/0014-longest-common-prefix/solution.cpp
+++ leetcode/0014-longest-common-prefix/solution.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
-        sort(strs.begin(),strs.end());
-        int n=strs.size();
-        string first=strs[0],last=strs[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
-            }ans+=first[i];
-
-        }return ans;
-
+        string prefix=strs[0];
+        for(int i=1;i<strs.size();i++){
+            while(strs[i].find(prefix)!=0){
+                 prefix = prefix.substr(0, prefix.size() - 1);
+                if (prefix.empty()) return "";
+            }
         }
-        
-
-        
-    
+        return prefix;
+    }
 };
