873d745df4f5daa0230bee0aeb536a6faa9c2bff
Paojangam kipgen <paojangam1234@gmail.com>
2025-09-13 16:01:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.7 MB (82.14%)

--- leetcode/0283-move-zeroes/solution.cpp
+++ leetcode/0283-move-zeroes/solution.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()<1) return ;
-        int *first=&nums[0];
-        
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        int left=0;
+        for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                swap(nums[i],*first);
-                first++;
+                swap(nums[left],nums[i]);
+                left++;
             }
         }
-        
-        
     }
 };
