5664c6687b161a5d882f57eb664c3f94ef6ebacd
Paojangam kipgen <paojangam1234@gmail.com>
2025-09-14 19:23:40 +0000

    [LeetCode Sync] Runtime - 3 ms (66.48%), Memory - 8.2 MB (11.06%)

--- leetcode/1013-fibonacci-number/solution.cpp
+++ leetcode/1013-fibonacci-number/solution.cpp
@@ -1,14 +1,18 @@
 class Solution {
-    int fibHelper(int n, vector<int> &memo){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        memo[n]=fibHelper(n-1,memo)+fibHelper(n-2,memo);
-        return memo[n];
-    }
 public:
     int fib(int n) {
-        vector<int>memo(n+1,-1);
-        return fibHelper(n,memo);
-        
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
+
