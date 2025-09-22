d89f7aa771086575fe10d8e53a4a534964ddb9d2
Paojangam kipgen <paojangam1234@gmail.com>
2025-09-22 13:42:06 +0000

    [LeetCode Sync] Runtime - 25 ms (44.37%), Memory - 107.4 MB (23.13%)

--- leetcode/0739-daily-temperatures/solution.cpp
+++ leetcode/0739-daily-temperatures/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
-        int n = t.size();
-        vector<int> ans(n,0);
-        stack<int> st; // stores indices
+        int n=t.size();
+        vector<int>answers (n,0);
+        stack<int>st;
         for(int i=0;i<n;i++){
-            while(!st.empty() && t[i] > t[st.top()]){
-                int idx = st.top(); st.pop();
-                ans[idx] = i - idx;
+            while(!st.empty() && t[i]>t[st.top()]){
+                int ind=st.top();
+                st.pop();
+                answers[ind]=i-ind;
             }
             st.push(i);
         }
-        return ans;
+        return answers;
     }
 };
 
