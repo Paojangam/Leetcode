bd2ea826fba6b9520e9872cec05cfad3e4885879
Paojangam kipgen <paojangam1234@gmail.com>
2025-09-22 15:06:32 +0000

    [LeetCode Sync] Runtime - 11 ms (99.00%), Memory - 92 MB (89.41%)

--- leetcode/0937-online-stock-span/solution.cpp
+++ leetcode/0937-online-stock-span/solution.cpp
@@ -1,13 +1,16 @@
 class StockSpanner {
-    stack<pair<int,int>> st; // pair: {price, span}
+    stack<pair<int,int>> st; 
+
 public:
-    StockSpanner() {}
+     StockSpanner() {}
 
     int next(int price) {
-        int span = 1;
-        while(!st.empty() && st.top().first <= price){
+         int span = 1;
+         while(!st.empty() && st.top().first <= price){
             span += st.top().second;
+
             st.pop();
+            
         }
         st.push({price, span});
         return span;
