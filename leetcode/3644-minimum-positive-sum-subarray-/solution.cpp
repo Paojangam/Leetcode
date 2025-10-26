eda83983d714f68c187cb6478bd576cb67a972ef
Paojangam kipgen <paojangam1234@gmail.com>
2025-10-26 06:42:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.4 MB (94.23%)

--- leetcode/3644-minimum-positive-sum-subarray-/solution.cpp
+++ leetcode/3644-minimum-positive-sum-subarray-/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int n = nums.size();
-        int minSum = INT_MAX;
-
-        for (int k = l; k <= r; ++k) {
-            int windowSum = 0;
-            for (int i = 0; i < k; ++i) {
-                windowSum += nums[i];
-            }
-            if (windowSum > 0) minSum = min(minSum, windowSum);
-
-            for (int i = k; i < n; ++i) {
-                windowSum += nums[i] - nums[i - k];
-                if (windowSum > 0) minSum = min(minSum, windowSum);
-            }
+       int n=nums.size();
+       int minsum=INT_MAX;
+       for(int k=l;k<=r;k++){
+        int windowsum=0;
+        for(int i=0;i<k;i++){
+            windowsum+=nums[i];
         }
+        if(windowsum>0) minsum=min(minsum,windowsum);
+        for(int i=k;i<n;i++){
+            windowsum+=nums[i]-nums[i-k];
+        if(windowsum>0) minsum=min(minsum,windowsum);
 
-        return (minSum == INT_MAX) ? -1 : minSum;
+        }
+       }
+       return (minsum == INT_MAX) ? -1 : minsum;
     }
 };
 
