62bbacbad87f0fafa530888206433199789eef6e
Paojangam kipgen <paojangam1234@gmail.com>
2025-11-19 07:22:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (73.58%)

--- leetcode/0747-min-cost-climbing-stairs/solution.cpp
+++ leetcode/0747-min-cost-climbing-stairs/solution.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-      int n=cost.size();
-      int prev2=cost[0];
-      int prev1=cost[1];
+        int n=cost.size();
+        int prev1=cost[0];
+        int prev2=cost[1];
+        for(int i=2;i<n;i++){
+            int cur = cost[i]+min(prev1,prev2);
+            prev1=prev2;
+            prev2=cur;
 
-      for(int i=2;i<n;i++){
-        int cur=cost[i]+min(prev1,prev2);
-        prev2=prev1;
-        prev1=cur;
-      } 
-      return min(prev1,prev2);
+        }
+        return min(prev1,prev2);
+
+      
     }
 };
 
